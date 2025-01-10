177f7e24a3816583eb70255811d02dac0fd150b1
Steven Pratama <dstevenpratama@gmail.com>
2025-01-10 20:16:02 +0700

    Version 1.4 Fixed Stopwatch Local Data Presistence

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -2,13 +2,18 @@ import React, { useState, useEffect, useRef } from "react";
 import "./Stopwatch.css";
 
 function Stopwatch() {
-  const [isRunning, setIsRunning] = useState(false);
-  const [elapsedTime, setElapsedTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(
+    () => JSON.parse(localStorage.getItem("isRunning")) || false
+  );
+  const [elapsedTime, setElapsedTime] = useState(
+    () => parseInt(localStorage.getItem("elapsedTime"), 10) || 0
+  );
   const intervalIdRef = useRef(null);
   const startTimeRef = useRef(0);
 
   useEffect(() => {
     if (isRunning) {
+      startTimeRef.current = Date.now() - elapsedTime;
       intervalIdRef.current = setInterval(() => {
         setElapsedTime(Date.now() - startTimeRef.current);
       }, 10);
@@ -18,6 +23,11 @@ function Stopwatch() {
       clearInterval(intervalIdRef.current);
     };
   }, [isRunning]);
+
+  useEffect(() => {
+    localStorage.setItem("isRunning", JSON.stringify(isRunning));
+    localStorage.setItem("elapsedTime", elapsedTime.toString());
+  }, [isRunning, elapsedTime]);
 
   function startTimer() {
     setIsRunning(true);
@@ -31,6 +41,8 @@ function Stopwatch() {
   function resetTimer() {
     setElapsedTime(0);
     setIsRunning(false);
+    localStorage.removeItem("elapsedTime");
+    localStorage.removeItem("isRunning");
   }
 
   function formatTime() {
