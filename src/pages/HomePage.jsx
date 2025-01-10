3f2c91047faf9963767924b2b3621e958b7238f4
Steven Pratama <dstevenpratama@gmail.com>
2025-01-10 20:34:59 +0700

    Version 1.5 Updated Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,28 @@
+import motivationImage from "../assets/motivation.png";
+
 function HomePage() {
   return (
-    <>
+    <div>
       <h1>Home Page</h1>
-    </>
+      <p>Hi! I'm Steven, I code this for fun. Welcome! ;)</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: "10vh",
+        }}
+      >
+        <img
+          src={motivationImage}
+          style={{
+            maxWidth: "60vw",
+            filter: "brightness(50%)",
+          }}
+          alt="Motivation Image"
+        />
+      </div>
+    </div>
   );
 }
 
